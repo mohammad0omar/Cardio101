866cdb818146759576fb415a091bd506a30926c6
moham <lead.dss.imperiumcode@gmail.com>
2021-12-04 17:46:03 +0200

    Add project files.

---
+++ Cardio101/Controllers/PatientController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cardio101.Controllers
+{
+    public class PatientController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
+    
