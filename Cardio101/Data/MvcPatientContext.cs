1e35a7c55fd886d974bbfec77358deff8cfc51c6
Mohammad Omar <mohammad0omar@outlook.com>
2021-12-04 21:52:15 +0200

    Add(patient): db context

---
+++ Cardio101/Data/MvcPatientContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Cardio101.Models;
+
+namespace Cardio101.Data
+{
+    public class MvcPatientContext : DbContext
+    {
+        public MvcPatientContext(DbContextOptions<MvcPatientContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Patient> Patient { get; set; }
+    }
+}
