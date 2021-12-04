a1a0e7de302bf0eb7b5233aebf2ae53f6b36c270
Mohammad Omar <mohammad0omar@outlook.com>
2021-12-05 01:27:11 +0200

    add(study): model

---
+++ Cardio101/Models/Study.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cardio101.Models
+{
+    public class Study
+    {
+        public int Id { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Start Time"), Required]
+        // [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public System.DateTime StartTime { get; set; }
+
+        [Display(Name = "Duration (in days)"), Required]
+        public uint Duration { get; set;}
+
+        [Display(Name = "Patient Name"), Required]
+        public Patient Patient { get; set; }
+
+        [Display(Name = "Device Serial Number"), Required]
+        public Device Device { get; set; }
+
+    }
+}
